fcd41bd69e249e5856430413496ff69791a81c94
Sambharya <abhisheksambharya05@gmail.com>
2024-03-29 01:49:31 +0530

    code Added

---
+++ models/transectionModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const transectionSchema = new mongoose.Schema({
+    userid:{
+        type:String,
+        required:true,
+    },
+    amount:{
+        type:Number,
+        required:[true,'Amount is Required']
+    },
+    type:{
+        type:String,
+        required:[true,'Type is Required']
+    },
+    category:{
+        type:String,
+        required:[true,'Category is Required']
+    },
+    reference:{
+        type:String,
+    },
+    description:{
+        type:String,
+        required:[true,'Description is Required']
+    },
+    date:{
+        type:Date,
+        required:[true,'Date is Required']
+    }
+},{timestamps:true});
+
+const transectionModel = mongoose.model('transections',transectionSchema);
+module.exports = transectionModel;
